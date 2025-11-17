1f0fea3b3fb15ea7b076d1c24a594a944320e6b1
juan-mezcla <juanmezcua96@gmail.com>
2025-11-17 16:40:19 +0100

    añadido parte que faltaba de leer el json creado.

--- accesoDatos/src/main/java/accesoDatos/accesoDatos/App.java
+++ accesoDatos/src/main/java/accesoDatos/accesoDatos/App.java
@@ -1,6 +1,7 @@
 package accesoDatos.accesoDatos;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -8,6 +9,7 @@ import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 
 public class App 
@@ -36,6 +38,21 @@ public class App
 	            }
 
 	            System.out.println("Archivo JSON creado correctamente.");
+	            
+	            
+	            try (FileReader reader = new FileReader("alumnos.json")) {
+
+	               
+	                List<Alumno> alumnos = gson.fromJson(reader, new TypeToken<List<Alumno>>(){}.getType());
+
+	            
+	                for (Alumno a : alumnos) {
+	                    System.out.println(a);
+	                }
+
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
 
 	        } catch (IOException e) {
 	            e.printStackTrace();
